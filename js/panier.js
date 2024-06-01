090861d9374e6a5e9c3bd5954271cfdb65d26307
Sajed <150829492+SajedMussallem@users.noreply.github.com>
2024-05-31 22:01:59 -0400

    Create panier.js

---
+++ js/panier.js
@@ -0,0 +1,24 @@
+// cart.js
+document.addEventListener('DOMContentLoaded', () => {
+    fetchPanier();
+});
+
+function fetchPanier() {
+    fetch('/api/panier')
+        .then(response => response.json())
+        .then(data => populatePanierTable(data))
+        .catch(error => console.error('Erreur lors de la récupération du panier:', error));
+}
+
+function populatePanierTable(panier) {
+    const tableBody = document.getElementById('panierTable').getElementsByTagName('tbody')[0];
+    panier.forEach(item => {
+        const row = tableBody.insertRow();
+        row.insertCell(0).textContent = item.titre;
+        row.insertCell(1).textContent = item.auteur;
+        row.insertCell(2).textContent = item.genre;
+        row.insertCell(3).textContent = item.date_publication;
+        row.insertCell(4).textContent = item.date_emprunt;
+        row.insertCell(5).textContent = item.date_retour;
+    });
+}
